dc7969729e80521a43e71c2f5c303f92de4e4b0f
MahammadMustafayev <tu7ekdm78@code.edu.az>
2023-01-29 18:10:55 +0400

    added weapon C#

---
+++ Weapon/Weapon/Models/Silah.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon.Models
+{
+    internal class Silah
+    {
+        private int _bulletcapacity;
+        public int BulletCapacity { 
+            get 
+            {
+               return _bulletcapacity;
+            } 
+            set 
+            {
+                if (value>0 && value<=100)
+                {
+                    _bulletcapacity = value;
+                }
+            } 
+        }
+        public int CurrentBulletCount { get; set; }
+        public double DischargeSecond { get; set; }
+        public bool FireMode { get; set; }
+
+        public Silah()
+        {
+
+        }
+        public Silah(int bulletCapacity,int bulletcount)
+        {
+            this.BulletCapacity = bulletCapacity;
+            this.CurrentBulletCount = bulletcount;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Silahin daraq tutumu:{BulletCapacity},Daraqdaki cari gulle sayi:{CurrentBulletCount}" +
+                $"Atis modu {FireMode}");
+        }
+        public void Shoot()
+        {
+            if (CurrentBulletCount>0)
+            {
+                --CurrentBulletCount;
+                Console.WriteLine("1 gulle atildi");
+                Console.WriteLine($"Silahdaki gulle sayi {CurrentBulletCount}");
+            }
+            else
+            {
+                Console.WriteLine("Sizin silahinizda gulle yoxdur");
+            }
+        }
+        public void Fire()
+        {
+            if (CurrentBulletCount>0)
+            {
+                
+                if (FireMode==true)
+                {
+                    //double fireTime=(CurrentBulletCount* DischargeSecond)/BulletCapacity;
+                    double time = 0.1;
+                    DischargeSecond = time * CurrentBulletCount;
+                    CurrentBulletCount= 0;
+                    Console.WriteLine($"Silahdaki butun gullelerin atilma vaxti {Math.Round(DischargeSecond,2)} saniye "); //
+                }
+                else
+                {
+                    --CurrentBulletCount;
+                    Console.WriteLine("1 gulle atildi");
+                    Console.WriteLine($"Silahdaki gulle sayi {CurrentBulletCount}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sizin silahinizda gulle yoxdur");
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - CurrentBulletCount; 
+        }
+        public void Reload()
+        {
+            if (BulletCapacity != CurrentBulletCount)
+            {
+                int needBulletCount = GetRemainBulletCount();
+                CurrentBulletCount += needBulletCount;
+                Console.WriteLine($"Daraqin yeniden doldurulduqdan sonraki gulle sayi {CurrentBulletCount}");
+            }
+            else
+            {
+                Console.WriteLine("Sizin daraqiniz artiq doludur");
+            }
+        }
+        public void ChangeFireMode()
+        {
+             
+            if (FireMode==true)
+            {
+                FireMode = false;
+                Console.WriteLine("Silah tekli moddadir");
+            }
+            else
+            {
+                FireMode = true;
+                Console.WriteLine("Silah avtomatik moddadir");
+            }
+        }
+        public void ChangeBulletCapacity(int newBulletCapacity)
+        {
+            this.BulletCapacity = newBulletCapacity;
+            this.CurrentBulletCount = _bulletcapacity;
+            Console.WriteLine($"Sizin silahiniz yeni daraq tutumu {_bulletcapacity} -dir");
+        }
+    }
+}
---
+++ Weapon/Weapon/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Weapon.Models;
+
+namespace Weapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletCapacity;
+            do
+            {
+                Console.WriteLine("Daraqin umumi tutumunu  Daxil Edin");
+                bulletCapacity=Convert.ToInt32(Console.ReadLine());
+            } while (bulletCapacity <= 0 || bulletCapacity>100);
+            int bulletCount;
+            do
+            {
+                Console.WriteLine("Gulle Sayi Daxil Edin");
+                bulletCount=Convert.ToInt32(Console.ReadLine());
+            } while (bulletCount<=0 || bulletCount>bulletCapacity);
+
+            Silah weapon = new Silah(bulletCapacity,bulletCount);
+            int choise;
+                Console.WriteLine("0-Info ucun");
+                Console.WriteLine("1-Shoot ucun");
+                Console.WriteLine("2-Fire ucun");
+                Console.WriteLine("3-GetRemainCount ucun");
+                Console.WriteLine("4-Reload ucun ");
+                Console.WriteLine("5-Atis modu ucun");
+                Console.WriteLine("6-Edit");
+                Console.WriteLine("7-Cixis");
+            do
+            {
+                Console.WriteLine("Bir secim edin");
+                choise=Convert.ToInt32(Console.ReadLine());
+                switch (choise)
+                {
+                    case 0:
+                        Console.WriteLine("-------------------");
+                        weapon.ShowInfo();
+                        Console.WriteLine("-------------------");
+                        break;
+                    case 1:
+                        Console.WriteLine("-------------------");
+                        weapon.Shoot();
+                        Console.WriteLine("-------------------");
+                        break;
+                    case 2:
+                        Console.WriteLine("-------------------");
+                        weapon.Fire();
+                        Console.WriteLine("-------------------");
+                        break;
+                    case 3:
+                        Console.WriteLine("-------------------");
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        Console.WriteLine("-------------------");
+                        break;
+                    case 4:
+                        Console.WriteLine("-------------------");
+                        weapon.Reload();
+                        Console.WriteLine("-------------------");
+                        break;
+                    case 5:
+                        Console.WriteLine("-------------------");
+                        weapon.ChangeFireMode();
+                        Console.WriteLine("-------------------");
+                        break;
+                    case 6:
+
+                        Console.WriteLine("Cixi ucun Q secin");
+                        Console.WriteLine("Tutumu deyismek ucun T secin");
+                        char editchoise;
+                        do
+                        {
+                            Console.WriteLine("Bir secim daxil edin");
+                            editchoise=Convert.ToChar(Console.ReadLine());
+                            switch (editchoise)
+                            {
+                                case 'T':
+                                    int newBulletCapacity;
+                                    do
+                                    {
+                                        Console.WriteLine("Yeniden daraq tutumunu daxil edin:");
+                                        newBulletCapacity=Convert.ToInt32(Console.ReadLine());
+                                    } while (newBulletCapacity<=0 || newBulletCapacity>120);
+                                    weapon.ChangeBulletCapacity(newBulletCapacity);
+                                    break;
+                                default:
+                                    Console.WriteLine("Bele bir secim yoxdur");
+                                    break;
+                            }
+                        } while (editchoise != 'Q');
+                        break; 
+                    case 7:
+                        Console.WriteLine("-------------------");
+                        Console.WriteLine("Saqolun");
+                        Console.WriteLine("-------------------");
+
+                        break;
+                    default:
+                        Console.WriteLine("Bele bir secim yoxdur");
+                        break;
+                }
+
+            } while (choise != 7);
+        }
+    }
+}
